6e86ec4e3b05a9c16141aefde176925da2a1ba8b
leone melo <leone.melo10@outlook.com>
2024-06-21 21:07:17 -0300

    fundamentos

---
+++ fundamentos/operadores.js
@@ -0,0 +1,40 @@
+/** 
+ * Operadores
+   - Aritméticos
+   - Aritimético de atribuição
+   - comparação
+   - Lógicos
+* @author Melo   
+*/
+
+console.clear()
+console.log("Operadores aritméticos")
+console.log("* | Multiplicação")
+console.log("/ | Divisão")
+console.log("+ | Soma")
+console.log("- | subtração")
+console.log("% | Modulo (sobra da divisão)")
+console.log("")
+console.log("Operadores aritméticos de atribuição")
+console.log("*= | multiplicação e atribuição")
+console.log("/= | divisão e atribuição")
+console.log("+= | soma e atribuição")
+console.log("-= | subtração e atribuição")
+console.log("++ | somar 1")
+console.log("-- | subtrair 1")
+console.log("")
+console.log("operadores de comparação")
+console.log(">   | maior que")
+console.log(">=  | maior ou igual a")
+console.log("-   | menor que")
+console.log("-=  | menor ou igual a")
+console.log("==  | igual a")
+console.log("!=  | diferente de")
+console.log("=== | identico a")
+console.log("!== | nao identico a")
+console.log("")
+console.log("operadores lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("!  | NOT")
+
